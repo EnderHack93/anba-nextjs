ee226bfc976ff9a13d3044240d8091cf173a0030
Mauro Rada <mrada1@bitbucket.org>
2024-11-18 09:00:08 -0400

    coomit error get params

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import Image from "next/image";
 import Swal from "sweetalert2";
 import { resetPassword } from "@/services/auth/reset-password/page";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { Suspense } from "react";
 
 const ResetPasswordPage = () => {
   const searchParams = useSearchParams();
-  const token = searchParams.get("token");
+  const router = useRouter();
+  const token = searchParams?.get("token");
   const [showPassword, setShowPassword] = useState(false);
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -26,10 +28,10 @@ const ResetPasswordPage = () => {
         confirmButtonText: "Volver al inicio",
         confirmButtonColor: "#4169E1",
       }).then(() => {
-        window.location.href = "/auth/login";
+        router.push("/auth/login");
       });
     }
-  }, [token]);
+  }, [token, router]);
 
   // Función para validar la contraseña
   const validatePassword = (password: string) => {
@@ -54,7 +56,7 @@ const ResetPasswordPage = () => {
     }
   }, [newPassword]);
 
-  const handleSubmit = async (e: any) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (newPassword !== confirmPassword) {
@@ -89,7 +91,7 @@ const ResetPasswordPage = () => {
             icon: "success",
             confirmButtonColor: "#50C878",
           }).then(() => {
-            window.location.href = "/auth/login";
+            router.push("/auth/login");
           });
         }
       }
@@ -106,114 +108,116 @@ const ResetPasswordPage = () => {
   };
 
   return (
-    <div className="relative flex min-h-screen items-center justify-center bg-gradient-to-br from-royalBlue-light p-4">
-      <div className="absolute inset-0 overflow-hidden">
-        <Image
-          src="/images/posrtada2.jpg"
-          alt="Background"
-          fill
-          className="object-cover blur-sm opacity-60"
-          priority
-        />
-      </div>
-      <div className="relative z-10 w-full max-w-sm bg-white p-6 rounded-xl shadow-xl bg-opacity-95 md:max-w-md">
-        <div className="text-center mb-8">
-          <h2 className="text-3xl font-bold text-royalBlue-darker">
-            Restablecer Contraseña
-          </h2>
-          <p className="text-gray-600 text-sm mt-2">
-            Ingrese tu nueva contraseña
-          </p>
+    <Suspense fallback={<div>Cargando...</div>}>
+      <div className="relative flex min-h-screen items-center justify-center bg-gradient-to-br from-royalBlue-light p-4">
+        <div className="absolute inset-0 overflow-hidden">
+          <Image
+            src="/images/posrtada2.jpg"
+            alt="Background"
+            fill
+            className="object-cover blur-sm opacity-60"
+            priority
+          />
         </div>
+        <div className="relative z-10 w-full max-w-sm bg-white p-6 rounded-xl shadow-xl bg-opacity-95 md:max-w-md">
+          <div className="text-center mb-8">
+            <h2 className="text-3xl font-bold text-royalBlue-darker">
+              Restablecer Contraseña
+            </h2>
+            <p className="text-gray-600 text-sm mt-2">
+              Ingresa tu nueva contraseña
+            </p>
+          </div>
 
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              htmlFor="newPassword"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Nueva Contraseña
-            </label>
-            <div className="relative">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label
+                htmlFor="newPassword"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Nueva Contraseña
+              </label>
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="newPassword"
+                  className={`w-full mt-1 px-4 py-2 border ${
+                    error ? "border-red-500" : "border-gray-300"
+                  } rounded-md shadow-sm focus:outline-none focus:ring-2 ${
+                    error ? "focus:ring-red-500" : "focus:ring-royalBlue"
+                  } focus:border-royalBlue transition duration-150`}
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  required
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute inset-y-0 right-0 flex items-center mt-1 px-3 text-gray-500 hover:text-gray-700"
+                >
+                  {showPassword ? (
+                    <FontAwesomeIcon icon={faEye} />
+                  ) : (
+                    <FontAwesomeIcon icon={faEyeSlash} />
+                  )}
+                </button>
+              </div>
+              {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="confirmPassword"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Confirmar Contraseña
+              </label>
               <input
-                type={showPassword ? "text" : "password"}
-                id="newPassword"
+                type="password"
+                id="confirmPassword"
                 className={`w-full mt-1 px-4 py-2 border ${
-                  error ? "border-red-500" : "border-gray-300"
+                  newPassword !== confirmPassword && confirmPassword
+                    ? "border-red-500"
+                    : "border-gray-300"
                 } rounded-md shadow-sm focus:outline-none focus:ring-2 ${
-                  error ? "focus:ring-red-500" : "focus:ring-royalBlue"
+                  newPassword !== confirmPassword && confirmPassword
+                    ? "focus:ring-red-500"
+                    : "focus:ring-royalBlue"
                 } focus:border-royalBlue transition duration-150`}
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
                 required
               />
-              <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute inset-y-0 right-0 flex items-center mt-1 px-3 text-gray-500 hover:text-gray-700"
-              >
-                {showPassword ? (
-                  <FontAwesomeIcon icon={faEye} />
-                ) : (
-                  <FontAwesomeIcon icon={faEyeSlash} />
-                )}
-              </button>
+              {newPassword !== confirmPassword && confirmPassword && (
+                <p className="text-red-500 text-sm mt-2">
+                  Las contraseñas no coinciden.
+                </p>
+              )}
             </div>
-            {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="confirmPassword"
-              className="block text-sm font-medium text-gray-700"
+            <button
+              type="submit"
+              disabled={
+                !!(isSubmitting || error || newPassword !== confirmPassword)
+              }
+              className={`w-full px-4 py-2 text-white font-semibold rounded-md ${
+                isSubmitting || error || newPassword !== confirmPassword
+                  ? "bg-gray-400 cursor-not-allowed"
+                  : "bg-blue-600 hover:bg-blue-700"
+              }`}
             >
-              Confirmar Contraseña
-            </label>
-            <input
-              type="password"
-              id="confirmPassword"
-              className={`w-full mt-1 px-4 py-2 border ${
-                newPassword !== confirmPassword && confirmPassword
-                  ? "border-red-500"
-                  : "border-gray-300"
-              } rounded-md shadow-sm focus:outline-none focus:ring-2 ${
-                newPassword !== confirmPassword && confirmPassword
-                  ? "focus:ring-red-500"
-                  : "focus:ring-royalBlue"
-              } focus:border-royalBlue transition duration-150`}
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
-            {newPassword !== confirmPassword && confirmPassword && (
-              <p className="text-red-500 text-sm mt-2">
-                Las contraseñas no coinciden.
-              </p>
-            )}
-          </div>
-          <button
-            type="submit"
-            disabled={
-              !!(isSubmitting || error || newPassword !== confirmPassword)
-            }
-            className={`w-full px-4 py-2 text-white font-semibold rounded-md ${
-              isSubmitting || error || newPassword !== confirmPassword
-                ? "bg-gray-400 cursor-not-allowed"
-                : "bg-blue-600 hover:bg-blue-700"
-            }`}
-          >
-            {isSubmitting ? "Restableciendo..." : "Restablecer Contraseña"}
-          </button>
+              {isSubmitting ? "Restableciendo..." : "Restablecer Contraseña"}
+            </button>
 
-          <button
-            type="button"
-            onClick={() => (window.location.href = "/auth/login")}
-            className="w-full px-4 py-2 mt-3 text-white font-semibold rounded-md bg-gray-400 hover:bg-gray-500"
-          >
-            Volver
-          </button>
-        </form>
+            <button
+              type="button"
+              onClick={() => router.push("/auth/login")}
+              className="w-full px-4 py-2 mt-3 text-white font-semibold rounded-md bg-gray-400 hover:bg-gray-500"
+            >
+              Volver
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
